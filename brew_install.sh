bba46e73869eb9c69b865e58c10204706eecc5a5
amar <er.amar@gmail.com>
2017-05-23 08:10:35 +0530

    Done with brew cask

--- brew_install.sh
+++ brew_install.sh
@@ -22,18 +22,7 @@ echo "Installing some brew packages"
 brew install wget automake autoconf libevent openssl libmemcached ossp-uuid xz libpng pkg-config icoutils libtool qt libtiff pstree ack curl imagemagick libxml2 readline dialog jpeg memcached redis freetype lftp node tmux autojump gawk git bash-completion git-flow hub colordiff android-sdk pidcat
 echo "done."
 
-# echo "installing quicklook cask plugins
-# brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
-# echo "done."
-
-# not using till brew cask is stable
-# use "brew cask audit <package_name>" to check installability of caks
-echo "Installing cask apps"
-#brew cask install adium google-chrome macvim superduper android-studio hexchat monolingual tagger anvil intel-haxm noizio the-unarchiver apptrap iterm2-borderless onyx transmission atom java picasa tunnelblick awareness kdiff3 qlvideo vienna battery-guardian keepingyouawake recordit vimr cakebrew launchrocket rowanj-gitx vlc cheatsheet loading shuttle xld flash-player mac2imgur sourcetree flashlight macpass subtitles dropbox sequel-pro skype cloudapp rdio disk-inventory-x slack
-# echo "done."
-
-
-echo "updating brew; brew-cask; cleaning up"
+echo "updating brew; cleaning up"
 brew update && brew upgrade && brew cleanup
 echo "done"
 

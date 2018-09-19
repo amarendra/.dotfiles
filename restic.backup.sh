179c956742fc22426e2421833d7a23ba6f2f2170
amar <amar@mac>
2018-09-19 12:47:08 +0530

    added path to launch agents, moved all logging to a common log file (including error logs)

--- restic.backup.sh
+++ restic.backup.sh
@@ -2,10 +2,7 @@
 # sources environment varialbes including credentials
 source $HOME/Dropbox/Config/git-ignored/restic/.restic.env
 
-# a new log file for every run so that I can easily check what happened when and can delete previous log files
-LOG_FILE=$RESTIC_LOG_DIR/restic_backup.log
-
-echo "RESTIC BACKUP SCRIPT STARTED AT $(date -R)" >> $LOG_FILE
+echo "RESTIC BACKUP SCRIPT STARTED AT $(date -R)" >> $RESTIC_LOG_FILE
 
 # regain lock if needed
 restic unlock
@@ -14,13 +11,13 @@ restic unlock
 restic --verbose backup \
 	--files-from $RESTIC_INCLUDE_FILE \
 	--exclude-caches \
-	--exclude-file=$RESTIC_EXCLUDE_FILE >> $LOG_FILE
+	--exclude-file=$RESTIC_EXCLUDE_FILE >> $RESTIC_LOG_FILE
 
-echo "Running restic check with cache...." >> $LOG_FILE
-restic check --with-cache >> $LOG_FILE
+echo "Running restic check with cache...." >> $RESTIC_LOG_FILE
+restic check --with-cache >> $RESTIC_LOG_FILE
 
-echo "Running restic stats...." >> $LOG_FILE
-restic stats >> $LOG_FILE
+echo "Running restic stats...." >> $RESTIC_LOG_FILE
+restic stats >> $RESTIC_LOG_FILE
 
 # reset restic credentials
 export RESTIC_REPOSITORY=""
@@ -28,8 +25,8 @@ export B2_ACCOUNT_ID=""
 export B2_ACCOUNT_KEY=""
 export RESTIC_PASSWORD_FILE=""
 
-echo "RESTIC BACKUP SCRIPT FINISHED $(date -R)" >> $LOG_FILE
+echo "RESTIC BACKUP SCRIPT FINISHED $(date -R)" >> $RESTIC_LOG_FILE
 
-echo "===================================================================" >> $LOG_FILE
+echo "===================================================================" >> $RESTIC_LOG_FILE
 
 exit 0
--- restic.maintain.sh
+++ restic.maintain.sh
@@ -2,24 +2,22 @@
 # sources environment varialbes including credentials
 source $HOME/Dropbox/Config/git-ignored/restic/.restic.env
 
-LOG_FILE=$RESTIC_LOG_DIR/restic_maintenance.log
-
-echo "RESTIC MAINTENANCE SCRIPT STARTED AT $(date -R)" >> $LOG_FILE
+echo "RESTIC MAINTENANCE SCRIPT STARTED AT $(date -R)" >> $RESTIC_LOG_FILE
 
 restic --verbose forget --keep-last 10 \
 	--keep-daily 7 \
 	--keep-weekly 5 \
 	--keep-monthly 12 \
 	--keep-yearly 3 \
-	--prune	>> $LOG_FILE
+	--prune	>> $RESTIC_LOG_FILE
 	
 # Checks the backup status. Once in a while run this without cache.
-echo "Running restic check...." >> $LOG_FILE
-restic check >> $LOG_FILE
+echo "Running restic check...." >> $RESTIC_LOG_FILE
+restic check >> $RESTIC_LOG_FILE
 
 
-echo "Running restic stats...." >> $LOG_FILE
-restic stats >> $LOG_FILE
+echo "Running restic stats...." >> $RESTIC_LOG_FILE
+restic stats >> $RESTIC_LOG_FILE
 
 # reset restic credentials
 export RESTIC_REPOSITORY=""
@@ -27,8 +25,8 @@ export B2_ACCOUNT_ID=""
 export B2_ACCOUNT_KEY=""
 export RESTIC_PASSWORD_FILE=""
 
-echo "RESTIC MAINTENANCE SCRIPT FINISHED at $(date -R)" >> $LOG_FILE
+echo "RESTIC MAINTENANCE SCRIPT FINISHED at $(date -R)" >> $RESTIC_LOG_FILE
 
-echo "===================================================================" >> $LOG_FILE
+echo "===================================================================" >> $RESTIC_LOG_FILE
 
 exit 0
